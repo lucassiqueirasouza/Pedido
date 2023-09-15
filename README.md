e6214f9521599a78fa47e7fc57d006909a856e1c
Lucas Siqueira Souza <91695925+lucassiqueirasouza@users.noreply.github.com>
2023-09-14 21:46:49 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pedido
+ Treinando javascript
---
+++ index.js
@@ -0,0 +1,15 @@
+function sim() {
+    alert('Aeee kkkk')
+}
+
+function desvia(t) {
+    var btn = t;
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(0, 90);
+    btn.style.lef = geraPosicao(0, 90);
+    console.log('opa, desviei...');
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + '%';
+}
